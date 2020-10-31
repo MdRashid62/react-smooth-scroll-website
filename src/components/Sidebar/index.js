ccc9d28e8b8ed4f15cf3df3268e44afd32d0d501
MdRashid62 <rashy.mr62@gmail.com>
2020-10-31 19:05:29 +0500

    styling sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { SidebarContainer, Icon, CloseIcon,  SidebarWrapper, SidebarMenu, SidebarLink, SidebarBtnWrap, SidebarRoute} from './SidebarElements';
 
 const Sidebar = () => {
     return (
@@ -6,6 +7,25 @@ const Sidebar = () => {
             <Icon>
                 <CloseIcon />
             </Icon>
+            <SidebarWrapper>
+                <SidebarMenu>
+                    <SidebarLink to="about">
+                        About
+                    </SidebarLink>
+                    <SidebarLink to="discover">
+                        Discover
+                    </SidebarLink>
+                    <SidebarLink to="services">
+                        Services
+                    </SidebarLink>
+                    <SidebarLink to="signup">
+                        Sign Up
+                    </SidebarLink>
+                </SidebarMenu>
+                <SidebarBtnWrap>
+                    <SidebarRoute to="/signin">Sign In</SidebarRoute>
+                </SidebarBtnWrap>
+            </SidebarWrapper>
         </SidebarContainer>
     )
 }
