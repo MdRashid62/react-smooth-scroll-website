b0093a5886af5c36b652401483a0614a61bc5757
MdRashid62 <rashy.mr62@gmail.com>
2020-10-31 15:54:18 +0500

    styling navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Nav, NavbarContainer, NavLogo} from './NavbarElements';
+import {FaBars} from 'react-icons/fa';
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink} from './NavbarElements';
 
 
 const Navbar = () => {
@@ -8,6 +9,26 @@ const Navbar = () => {
           <Nav>
             <NavbarContainer >
                 <NavLogo to='/'> Dolla </NavLogo>
+                <MobileIcon>
+                    <FaBars />
+                </MobileIcon>
+                <NavMenu>
+                    <NavItem>
+                        <NavLinks to="about">About</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="discover">Discover</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="services">Services</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="signup">Sign Up</NavLinks>
+                    </NavItem>
+                </NavMenu>
+                <NavBtn>
+                    <NavBtnLink to="/signin">Sign In</NavBtnLink>
+                </NavBtn>
             </NavbarContainer>
           </Nav>
         </>
