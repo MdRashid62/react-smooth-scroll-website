6aaca731e59f94a3508fae55e0043cecbe8925ee
MdRashid62 <rashy.mr62@gmail.com>
2020-10-31 14:52:13 +0500

    navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Nav, NavbarContainer, NavLogo} from './NavbarElements';
+
 
 const Navbar = () => {
     return (
         <>
-            <Nav>
-                <NavbarContainer>
-                    <NavLogo>
-                        Dolla
-                    </NavLogo>
-                </NavbarContainer>
-            </Nav>
+          <Nav>
+            <NavbarContainer >
+                <NavLogo to='/'> Dolla </NavLogo>
+            </NavbarContainer>
+          </Nav>
         </>
     )
 }
 
-export default Navbar
+export default Navbar;
